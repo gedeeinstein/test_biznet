6f36378e4f72b9c98abe2a2bdf03c7140f67e97e
gedeeinstein <gedeeinstein@gmail.com>
2020-10-15 13:59:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 
 let data_old = [{ "username": "ali", "hair_color": "brown", "height": 1.2 }, { "username": "marc", "hair_color": "blue", "height": 1.4 }, { "username": "joe", "hair_color": "brown", "height": 1.7 }, { "username": "zehua", "hair_color": "black", "height": 1.8 }];
 
-let data_new = {"h":["username","hair_color","height"],"d":[["ali","brown",1.2],["mar c","blue",1.4],["joe","brown",1.7],["zehua","black",1.8]]};
+// let data_new = {"h":["username","hair_color","height"],"d":[["ali","brown",1.2],["mar c","blue",1.4],["joe","brown",1.7],["zehua","black",1.8]]};
 // let data_x = JSON.stringify(data_new);
-let data_x = JSON.parse(JSON.stringify(data_new));
+let data_x = JSON.parse(JSON.stringify(data_old));
 
 const compress = (data = [], h = Object .keys (data [0] || {})) => 
   ({h, d: data .map (o => h .map (key => o [key]))})
@@ -18,7 +18,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/data_test', (req, res) => {
-    res.send(data_old)
+    res.send(data_x)
+    console.log(data_x)
 });
 
 app.get('/api/data_new', (req, res) => {
