dbca005ad6daea7680a10ab654011f66e0881c1c
gedeeinstein <gedeeinstein@gmail.com>
2020-10-15 13:57:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const app = express();
 let data_old = [{ "username": "ali", "hair_color": "brown", "height": 1.2 }, { "username": "marc", "hair_color": "blue", "height": 1.4 }, { "username": "joe", "hair_color": "brown", "height": 1.7 }, { "username": "zehua", "hair_color": "black", "height": 1.8 }];
 
 let data_new = {"h":["username","hair_color","height"],"d":[["ali","brown",1.2],["mar c","blue",1.4],["joe","brown",1.7],["zehua","black",1.8]]};
+// let data_x = JSON.stringify(data_new);
+let data_x = JSON.parse(JSON.stringify(data_new));
+
+const compress = (data = [], h = Object .keys (data [0] || {})) => 
+  ({h, d: data .map (o => h .map (key => o [key]))})
+
+console.log (compress (data_old));
+
 
 app.get('/', (req, res) => {
     res.send('Hellow World');
@@ -14,7 +22,7 @@ app.get('/api/data_test', (req, res) => {
 });
 
 app.get('/api/data_new', (req, res) => {
-    res.send(data_new)
+    res.send(data_x)
 });
 
 
